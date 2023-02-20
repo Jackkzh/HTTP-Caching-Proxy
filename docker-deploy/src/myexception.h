047ec48600ba61b9f9d81a19ff50d660d61e2187
YUME-FF <zf70@duke.edu>
2023-02-19 20:49:07 -0500

    add myexception.h

---
+++ docker-deploy/src/myexception.h
@@ -0,0 +1,17 @@
+#ifndef __MYEXCEPTION__H__
+#define __MYEXCEPTION__H__
+#include <exception>
+#include <string>
+
+using namespace std;
+
+class myException : public exception {
+ private:
+  string msg;
+
+ public:
+  explicit myException(string message) : msg(message) {}
+
+  virtual const char * what() { return msg.c_str(); }
+};
+#endif
